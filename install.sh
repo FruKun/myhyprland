60160dda069ba8ef21a998b24d2727b4b3d2fe4c
FruKun <127653755+FruKun@users.noreply.github.com>
2025-01-19 12:41:22 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 cd
-git clone https://aur.archlinux.org/yay.git
-cd yay
+sudo pacman -S --needed base-devel
+git clone https://aur.archlinux.org/paru.git
+cd paru
 makepkg -si
-yay
+paru
 cd
-yay -S --needed hyprland chromium code vim neovim nano thunar kitty swaylock firefox man pavucontrol brightnessctl dunst grit imv noto-fonts noto-fonts-emoji noto-fonts-cjk waybar udiskie ttf-font-awesome ttf-jetbrains-mono-nerd tlp tlp-rdw acpid swayidle swaybg slurp ranger qt6ct qt5ct playerctl pipewire nwg-look nm-applet blueman wofi catppuccin-gtk-theme-mocha catppuccin-cursors-mocha wl-clipboard cliphist xdg-desktop-portal-hyprland sway-audio-idle-inhibit-git-debug python-requests polkit-kde-agent tela-circle-icon-theme-dracula
+paru -S --needed --noconfirm hyprland chromium code vim neovim nano thunar kitty swaylock firefox man pavucontrol brightnessctl dunst grit imv noto-fonts noto-fonts-emoji noto-fonts-cjk waybar udiskie ttf-font-awesome ttf-jetbrains-mono-nerd tlp tlp-rdw acpid swayidle swaybg slurp ranger qt6ct qt5ct playerctl pipewire nwg-look nm-applet blueman wofi catppuccin-gtk-theme-mocha catppuccin-cursors-mocha wl-clipboard cliphist xdg-desktop-portal-hyprland sway-audio-idle-inhibit-git-debug python-requests polkit-kde-agent tela-circle-icon-theme-dracula
 systemctl enable bluetooth.service
 systemctl enable tlp.service
 sudo sh -c 'echo "HandlePowerKey=suspend" >> /etc/systemd/logind.conf'
