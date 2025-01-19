3859dccc41036fc6e73b01df440aff602b6a337a
frukun <peromata@yandex.ru>
2025-01-19 12:56:16 +0500

    initial commit

--- install.sh
+++ install.sh
@@ -5,8 +5,8 @@ cd yay
 makepkg -si
 yay
 cd
-git clone https://github.com/FruKun/myhyprarch.git
 yay -S --needed hyprland chromium code vim neovim nano thunar kitty swaylock firefox man pavucontrol brightnessctl dunst grit imv noto-fonts noto-fonts-emoji noto-fonts-cjk waybar udiskie ttf-font-awesome ttf-jetbrains-mono-nerd tlp tlp-rdw acpid swayidle swaybg slurp ranger qt6ct qt5ct playerctl pipewire nwg-look nm-applet blueman wofi catppuccin-gtk-theme-mocha catppuccin-cursors-mocha wl-clipboard cliphist xdg-desktop-portal-hyprland sway-audio-idle-inhibit-git-debug python-requests polkit-kde-agent tela-circle-icon-theme-dracula
 systemctl enable bluetooth.service
 systemctl enable tlp.service
 sudo sh -c 'echo "HandlePowerKey=suspend" >> /etc/systemd/logind.conf'
+cp -r ~/myhyprland/config/. ~/.config/
