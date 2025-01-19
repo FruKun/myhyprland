b282890dbc7cd8b4f7611892000e557170beb574
frukun <peromata@yandex.ru>
2025-01-19 12:48:12 +0500

    initial commit

---
+++ autogit.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+cp ~/.config/chromium-flags.conf ~/myhyprland/config/
+cp ~/.config/code-flags.conf ~/myhyprland/config/
+cp ~/.config/electron-flags.conf ~/myhyprland/config/
+cp -r ~/.config/hypr ~/myhyprland/config/
+cp -r ~/.config/kitty ~/myhyprland/config/
+cp -r ~/.config/waybar ~/myhyprland/config/
+cp -r ~/.config/dunst ~/myhyprland/config/
+cp -r ~/.config/wofi ~/myhyprland/config/
+cp -r ~/.config/swaylock ~/myhyprland/config/
+
+git add .
+git commit -m "update"
+git push i3wm main
